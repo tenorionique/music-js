baf3614ddfbafa219de76c0eb251c7be766ed9d6
monique <monique.sa@raizen.com>
2022-06-17 12:06:55 -0300

    aula 4 - funções anonimas

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
 while( contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom')
+    }
 
     contador = contador + 1;
     console.log(contador);
