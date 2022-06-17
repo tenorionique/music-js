546a29b38df5b1ed27ac80d96088a65523ff4288
monique <monique.sa@raizen.com>
2022-06-17 12:34:27 -0300

    aula 4 - repeticao otimizada com for

--- main.js
+++ main.js
@@ -4,21 +4,13 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while( contador < listaDeTeclas.length) {
+for ( let contador = 0; contador < listaDeTeclas.length; contador++ ) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-   
-    //template string
-    const idAudio = `#som_${instrumento}`;
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`;//template string
 
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
-
-    contador = contador + 1;
-    //console.log(contador);
-}
+};
