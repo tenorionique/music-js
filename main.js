e5d83f5bba8203ecf4a4634aba41d96c443a7d65
monique <monique.sa@raizen.com>
2022-06-17 12:48:33 -0300

    aula 5 - eventos do teclado e adicionando e removendo classes

--- main.js
+++ main.js
@@ -13,4 +13,12 @@ for ( let contador = 0; contador < listaDeTeclas.length; contador++ ) {
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup =  function () {
+        tecla.classList.remove('ativa')
+    }
 };
